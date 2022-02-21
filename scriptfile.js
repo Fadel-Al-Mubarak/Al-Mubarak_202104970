1876f337a5e32603614bfec727c72b513cbfac86
Fadel-Al-Mubarak <g202104970@kfupm.edu.sa>
2022-02-21 16:37:56 +0300

    commit Last updates HW2

--- scriptfile.js
+++ scriptfile.js
@@ -1,7 +1,7 @@
 function addTask(){
     var str =document.getElementById("text").value;
     const h2Elem = document.createElement('h2');
-    const div2 = document.querySelector('.secondDiv');
+    const div2 = document.querySelector('.thirdDiv');
     h2Elem.innerHTML = str;
     div2.append(h2Elem);
 }
